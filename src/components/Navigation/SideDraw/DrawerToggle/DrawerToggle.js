a9fa02c3b143d6c951c00c962a279607cef2889f
CornetS28 <corsam28@gmail.com>
2020-01-13 22:20:25 -0500

    adding hamburgur Icon for small screen device

--- src/components/Navigation/SideDraw/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDraw/DrawerToggle/DrawerToggle.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React from "react";
+import classes from "./DrawerToggle.css";
 
-const drawToggle = props  => (
-  <div onClick={props.clicked}>MENU</div>
+const drawToggle = props => (
+  <div className={classes.DrawerToggle} onClick={props.clicked}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
 );
-
 
 export default drawToggle;
